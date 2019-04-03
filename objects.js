4de7873d8a064372e189f638c7b8a8389b1e05e7
Resham Arora <resham.arora@gmail.com>
2019-04-03 00:56:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 } 
 
 function removeFromPlaylist(playlist, artistName0) {
-  delete playlist[artistName]
+  delete playlist.artistName
   return playlist
 }
